3b5c02b5273045e2e158f4eb1d77c17f0f9c3b0b
pablodco <100701377+pablodco@users.noreply.github.com>
2024-04-08 21:41:09 +0200

    Maven setup complete modules

---
+++ RouteBusParent/src/main/java/com/RouteBus/App.java
@@ -0,0 +1,13 @@
+package com.RouteBus;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
