5bfbfa71ca74cc932b4fc394aab42ed1c98eb007
DaddyShortMember <Marioepf@Opendeusto.es>
2024-05-02 06:10:47 +0200

    Update AdministratorWindow.java

--- RouteBusParent/Client/src/main/java/com/RouteBus/client/gui/AdministratorWindow.java
+++ RouteBusParent/Client/src/main/java/com/RouteBus/client/gui/AdministratorWindow.java
@@ -15,6 +15,8 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JList;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
 import javax.swing.JTextField;
 import javax.swing.SwingConstants;
 
@@ -26,14 +28,24 @@ public class AdministratorWindow extends JFrame{
     private static final long serialVersionUID = 1L;
 	private ServerGateway serverGateway;
 	private JLabel appNameLabel;
+
+	//Subtitle bar
+	private JMenuBar menuBar;
+	private JMenu bizInt;
+	private JMenu routeAdmin;
+    private JMenu busAdmin;
+    private JMenu ticketsAdmin;
+	private JMenu logout;
 	
 	//Most used bus(es)
 	private JLabel stat1Label;
 	private JList stat1Info; //Turn into JTextField if all else fails
+	private String busList[] = {"Bus 1", "Bus 2"};
 
 	//Most purchased rout(es)
 	private JLabel stat2Label;
 	private JList stat2Info; //Ditto.
+	private String routeList[] = {"Route 1", "Route 2"};
 	
 	//Total revenue
 	private JLabel stat3Label;
@@ -46,16 +58,23 @@ public class AdministratorWindow extends JFrame{
 		this.setLayout(null);
 		this.setBounds(500, 500, 800, 600);
 		this.setResizable(false);
+		this.setBackground(new Color(204, 153, 255));
+
+		//MenuBar
+		menuBar = new JMenuBar();
+		bizInt = new JMenu("Administrative View");
+		routeAdmin = new JMenu("Route Administration");
+		busAdmin = new JMenu("Bus Administration");
+		ticketsAdmin = new JMenu("Tickets Administration");
+		logout = new JMenu("Log Out");
 		
-		URL urlBackGround;
-		
-		try {
-			urlBackGround = new URL("https://www.caf.com/media/6860/las-carreteras-de-america-latina-no-estan-suficientemente-preparadas-para-enfrentar-el-cambio-climatico.jpg");
-			Image imageBackground = ImageIO.read(urlBackGround).getScaledInstance(400, 600, java.awt.Image.SCALE_SMOOTH);
-			this.setContentPane(new JLabel(new ImageIcon(imageBackground)));
-		} catch (Exception e) {
-			System.out.println("# Problem while setting the background: " + e);
-		}
+		menuBar.add(bizInt);
+		menuBar.add(routeAdmin);
+		menuBar.add(busAdmin);
+		menuBar.add(ticketsAdmin);
+		menuBar.add(logout);
+
+		this.setJMenuBar(menuBar);
 		
 		// ELEMENT CREATION
 		appNameLabel = new JLabel("RouteBus - Integrated Business Intelligence Module (IBIM)");
@@ -63,13 +82,16 @@ public class AdministratorWindow extends JFrame{
 		appNameLabel.setForeground(Color.WHITE);
         appNameLabel.setHorizontalAlignment(SwingConstants.CENTER);
 		
-		//Most 
+		//Most used buses
 		stat1Label = new JLabel("Most used buses:");
 		stat1Label.setForeground(Color.WHITE);
+		stat1Info = new JList(busList); //List of buses goes in
 		
 		//Most purchased routes
 		stat2Label = new JLabel("Most purchased routes:");
 		stat2Label.setForeground(Color.WHITE);
+		stat2Info = new JList(routeList); //List of routes goes in
+		
 		
 		//Total revenue;
 		stat3Label = new JLabel("Total Revenue:");
@@ -78,6 +100,7 @@ public class AdministratorWindow extends JFrame{
 		stat3Info.setEditable(false);
 		stat3Info.setText("$1");
 
+		//Adding things...
         appNameLabel.setBounds(0, 100, getWidth(), 30);
         this.add(appNameLabel);
         
@@ -88,6 +111,15 @@ public class AdministratorWindow extends JFrame{
 		this.add(stat2Label);
 
 		stat3Label.setBounds(140, 320, 150, 30);
+		this.add(stat3Label);
+
+		stat1Info.setBounds(240, 180, 150, 30);
+		this.add(stat1Label);
+
+		stat2Info.setBounds(240, 250, 150, 30);
+		this.add(stat2Label);
+
+		stat3Info.setBounds(240, 320, 30, 30);
 		this.add(stat3Label);
 
 		this.revalidate();
