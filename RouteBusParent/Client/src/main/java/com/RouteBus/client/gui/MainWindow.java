83dde7c115d288f3fc708f46cb591097f50da15b
Ainhoa Murcia <murcia.ainhoa@opendeusto.es>
2024-05-04 11:59:12 +0200

    Upgrade de la Main Window, el message propierties actualizado + basque

--- RouteBusParent/Client/src/main/java/com/RouteBus/client/gui/MainWindow.java
+++ RouteBusParent/Client/src/main/java/com/RouteBus/client/gui/MainWindow.java
@@ -21,14 +21,26 @@ public class MainWindow extends JFrame {
     
     private static final long serialVersionUID = 1L;
     
+    // Panels
+    private JPanel contentPanel;
+    private JPanel newsPanel;
+    private JPanel infoPanel;
+    
+    // Menu
     private JMenuBar menuBar;
     
+    private JMenu busRouteInfo;
     private JMenu myInformation;
     private JMenu myRoutes;
     private JMenu myBusTickets;
     private JMenu language;
     private JMenu logout;
     
+	private JMenuItem information;
+	private JMenuItem busInfo;
+	private JMenuItem routes;
+	private JMenuItem tickets;
+	private JMenuItem out;
     private JMenuItem spanish;
     private JMenuItem english;
     private JMenuItem basque;
@@ -36,12 +48,25 @@ public class MainWindow extends JFrame {
     
     private JTextArea noticia1;
     private JTextArea noticia2;
-	
-	
-    private JPanel contentPanel;
-    private JPanel newsPanel;
-    private JPanel infoPanel;
     
+    private JLabel lUserPic;
+    private JLabel lName;
+	private JLabel lSurname;
+	private JLabel lEmail;
+	private JLabel lBirthDay;
+	private JLabel lNationality;
+
+	
+	private JTextField tName;
+	private JTextField tSurname;
+	private JTextField tEmail;
+	private JTextField tID;
+	
+	private JButton bEdit;
+	private JDateChooser dBirthDate;
+	
+	private JComboBox<String> comboNationality;
+	
 	private ResourceBundle messages;
     
     public MainWindow() {
@@ -62,38 +87,62 @@ public class MainWindow extends JFrame {
         
         infoPanel = new JPanel(null);
         infoPanel.setBounds(0, 0, this.getWidth(), this.getHeight());
-        infoPanel.setBackground(Color.BLACK);
+        infoPanel.setBackground(Color.WHITE);
         
         contentPanel.add(newsPanel);
         
+        // Menu
         menuBar = new JMenuBar();
         menuBar.setBackground(new Color(204, 153, 255));
         
-        new JMenu(messages.getString("userMenu"));
+        //new JMenu(messages.getString("userMenu"));
+        busRouteInfo = new JMenu(messages.getString("BusRouteInfo"));
         myInformation = new JMenu(messages.getString("myInformationMenu"));
         myRoutes = new JMenu(messages.getString("myRoutesMenu"));
         myBusTickets = new JMenu(messages.getString("myBusTicketsMenu"));
         language = new JMenu(messages.getString("languageMenu"));
-        logout = new JMenu(messages.getString("logoutMenu"));
+        logout = new JMenu(messages.getString("OptionsMenu"));
+        
+        busInfo = new JMenuItem(messages.getString("News"));
+        information = new JMenuItem(messages.getString("MyInformation"));
+        routes = new JMenuItem(messages.getString("MyRoutes"));
+        tickets = new JMenuItem(messages.getString("MyTickets"));
+        out = new JMenuItem(messages.getString("Logout"));
         
         spanish = new JMenuItem(messages.getString("spanish"));
         english = new JMenuItem(messages.getString("english"));
         basque = new JMenuItem(messages.getString("basque"));
         german = new JMenuItem(messages.getString("german")); 
+        
         // Add menu items to the user menu
+        busRouteInfo.add(busInfo);
+        myInformation.add(information);
+        myRoutes.add(routes);
+        myBusTickets.add(tickets);
+        logout.add(out);
+        
+        // Add languages
+        language.add(basque);
+        language.add(english);
+        language.add(german);
+        language.add(spanish);
+        
+        menuBar.add(busRouteInfo);
         menuBar.add(myInformation);
         menuBar.add(myRoutes);
         menuBar.add(myBusTickets);
         menuBar.add(language);
         menuBar.add(logout);
         
-        language.add(spanish);
-        language.add(english);
-        language.add(basque);
-        language.add(german);
         this.setJMenuBar(menuBar);
-                
+        
+        // News Panel
         // Initialize text areas for news
+        JLabel titleLabel = new JLabel(messages.getString("welcome"));
+        titleLabel.setFont(new Font("Arial", Font.BOLD, 20)); // Set font and size
+        titleLabel.setForeground(Color.BLACK); // Set text color
+        titleLabel.setBounds(300, 50, 300, 30); // Adjust position and size as needed
+        newsPanel.add(titleLabel);
         
         noticia1 = new JTextArea(messages.getString("new1Content"));
         noticia1.setEditable(false); // Make it read-only
@@ -116,13 +165,69 @@ public class MainWindow extends JFrame {
         newsPanel.add(scrollPane1);
         newsPanel.add(scrollPane2);
         
-        myInformation.addActionListener(new ActionListener() {
+        // Information Panel
+        // title
+        JLabel infotitle = new JLabel(messages.getString("persInfoWelcome"));
+        infotitle.setFont(new Font("Arial", Font.BOLD, 20)); 
+        infotitle.setForeground(Color.BLACK); 
+        infotitle.setBounds(300, 50, 250, 50); 
+        infoPanel.add(infotitle);
+        
+        // labels
+        lUserPic = new JLabel(messages.getString("userPic"));
+        lUserPic.setBounds(90, 370, 150,30);
+
+        lName = new JLabel(messages.getString("nameLabel"));
+        lName.setBounds(350, 110, 150, 30);
+        lSurname = new JLabel(messages.getString("surnameLabel"));
+        lSurname.setBounds(350, 170, 150, 30);
+        lBirthDay = new JLabel(messages.getString("birthDateLabel"));
+        lBirthDay.setBounds(350, 230, 150,30);
+        lNationality = new JLabel(messages.getString("nationalityLabel"));
+        lNationality.setBounds(350, 290, 150,30);
+        lEmail = new JLabel(messages.getString("email"));
+        lEmail.setBounds(350,350, 150,30);
+        
+        infoPanel.add(lUserPic);
+        infoPanel.add(lName);
+        infoPanel.add(lSurname);
+        infoPanel.add(lBirthDay);
+        infoPanel.add(lNationality);
+        infoPanel.add(lEmail);
+        
+        // fields
+        tName = new JTextField();
+        tName.setBounds(350, 140, 150, 30);
+        tSurname = new JTextField();
+        tSurname.setBounds(350,200,150,30);
+        dBirthDate = new JDateChooser("yyyy/MM/dd", "####/##/##", '_');
+        dBirthDate.setBounds(350, 260, 150, 30);
+        comboNationality = new JComboBox<String>();
+        comboNationality.setBounds(350, 320, 150,30);
+        //loadNationalities();
+        tEmail = new JTextField();
+        tEmail.setBounds(350, 380, 150, 30);
+        
+        infoPanel.add(tName);
+        infoPanel.add(tSurname);
+        infoPanel.add(dBirthDate);
+        infoPanel.add(comboNationality);
+        infoPanel.add(tEmail);
+        
+        // button
+        bEdit = new JButton("Edit");
+        bEdit.setBackground(new Color(204, 153, 255));
+        bEdit.setBorder(null);
+        bEdit.setBounds(370, 440,100,30);
+        infoPanel.add(bEdit);
+        
+        // Actions
+        information.addActionListener(new ActionListener() {
 			
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				// TODO Auto-generated method stub
-                // Remove the news panel
-                contentPanel.remove(newsPanel);
+				getContentPane().removeAll();
                 // Add the user info panel
                 contentPanel.add(infoPanel);
                 // Repaint the content panel
@@ -132,42 +237,55 @@ public class MainWindow extends JFrame {
 			}
 		});
         
-        JLabel titleLabel = new JLabel("Welcome back!");
-        titleLabel.setFont(new Font("Arial", Font.BOLD, 20)); // Set font and size
-        titleLabel.setForeground(Color.BLACK); // Set text color
-        titleLabel.setBounds(350, 50, 200, 30); // Adjust position and size as needed
-        newsPanel.add(titleLabel);
+        busInfo.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// TODO Auto-generated method stub
+                // Remove the news panel
+                getContentPane().removeAll();
+                // Add the user info panel
+                contentPanel.add(newsPanel);
+                // Repaint the content panel
+                contentPanel.revalidate();
+                contentPanel.repaint();
+			}
+		});
         
+        // Images
         try {
             BufferedImage logo = ImageIO.read(getClass().getResource("/images/icon.jpg"));
             BufferedImage imag1 = ImageIO.read(getClass().getResource("/images/imag1.jpg"));
             BufferedImage imag2 = ImageIO.read(getClass().getResource("/images/imag2.jpg"));
+            BufferedImage imagUser = ImageIO.read(getClass().getResource("/images/imagUser.jpg"));
             
             ImageIcon icon = new ImageIcon(logo);
             ImageIcon icon2 = new ImageIcon(logo);
             ImageIcon imagIcon1 = new ImageIcon(imag1);
             ImageIcon imagIcon2 = new ImageIcon(imag2);
+            ImageIcon imagIconU = new ImageIcon(imagUser);
             
             JLabel imageLabel = new JLabel(icon);
             JLabel imageLabel2 = new JLabel(icon2);
             JLabel imageLabel3 = new JLabel(imagIcon1);
             JLabel imageLabel4 = new JLabel(imagIcon2);
+            JLabel imageLabelU = new JLabel(imagIconU);
             
             imageLabel.setBounds(590, -70, 200, 300); // Adjust position and size as needed
             imageLabel2.setBounds(590,-70, 200, 300);
             imageLabel3.setBounds(30,180,140,100);
             imageLabel4.setBounds(30,340,140,100);
+            imageLabelU.setBounds(50, 160, 200, 200);
             
             newsPanel.add(imageLabel);
             newsPanel.add(imageLabel3);
             newsPanel.add(imageLabel4);
             infoPanel.add(imageLabel2);
+            infoPanel.add(imageLabelU);
+            
         } catch (IOException e) {
             e.printStackTrace();
         }
-        
-        
-
         
         this.setVisible(true);
     }
